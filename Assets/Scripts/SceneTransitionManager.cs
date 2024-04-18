6dfe87c6be754ae7a754ec080cfb1d0515914355
bismayesh <125033480+bismayesh@users.noreply.github.com>
2024-04-18 14:45:16 +0200

    poke interactable

--- Assets/Scripts/SceneTransitionManager.cs
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -1,24 +1,48 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransitionManager : MonoBehaviour
 {
     public FadeScreen fadeScreen;
+    public float delayBeforeTransition = 3f; // Adjust this value to set the delay before transition
+    public int targetSceneIndex = 1; // Index of the target scene in the build settings
 
-    public void GotoScene(int sceneIndex)
-
+    void Start()
     {
-        StartCoroutine(GoToSceneRoutine(sceneIndex));
+        // Start the scene transition after the specified delay
+        StartCoroutine(StartTransitionAfterDelay());
     }
-    
-    IEnumerator GoToSceneRoutine(int sceneIndex)
 
+    IEnumerator StartTransitionAfterDelay()
+    {
+        // Wait for the specified delay before starting the transition
+        yield return new WaitForSeconds(delayBeforeTransition);
+
+        // Start the scene transition
+        yield return StartCoroutine(GoToSceneAsyncRoutine(targetSceneIndex));
+    }
+
+    IEnumerator GoToSceneAsyncRoutine(int sceneIndex)
     {
         fadeScreen.FadeOut();
-        yield return new WaitForSeconds(fadeScreen.fadeDuration);
-    }
-    
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
 
-     
+        float timer = 0;
+        while (timer <= fadeScreen.fadeDuration && !operation.isDone)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        operation.allowSceneActivation = true;
+
+        // Wait for the new scene to finish loading before unloading the current scene
+        yield return new WaitUntil(() => operation.isDone && SceneManager.GetSceneByBuildIndex(sceneIndex).isLoaded);
+
+        // Unload the current scene (Scene 1)
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
 }
+
